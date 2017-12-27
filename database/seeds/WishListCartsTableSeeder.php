7daa755664bfe7996542673e121af380948280ce
mahmoud20120366 <engmahmoudmohamed779@gail.com>
2017-12-27 23:24:57 +0200

    second

--- database/seeds/WishListCartsTableSeeder.php
+++ database/seeds/WishListCartsTableSeeder.php
@@ -12,5 +12,20 @@ class WishListCartsTableSeeder extends Seeder
     public function run()
     {
         //
+        $user_ids = DB::table('users')->pluck('id');
+        $product_ids = DB::table('products')->pluck('id');
+        $number_of_products = rand(0, 5);
+        $user_index = rand(0, count($user_ids)-1);
+
+        $id = DB::table('wish_carts')->insertGetId([
+            'user_id' => $user_ids[$user_index]
+        ]);
+
+        for ($i = 0; $i< $number_of_products ; $i++){
+            DB::table('wish_products')->insert([
+                'cart_id' => $id,
+                'product_id' => $product_ids[rand(0, count($product_ids)-1)]
+            ]);
+        }
     }
 }
