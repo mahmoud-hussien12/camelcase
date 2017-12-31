6b9bfa52b5219d489b3227a473ce98e8177815b2
mahmoud20120366 <engmahmoudmohamed779@gail.com>
2017-12-31 22:16:15 +0200

    second

--- database/seeds/OrdersTableSeeder.php
+++ database/seeds/OrdersTableSeeder.php
@@ -12,5 +12,22 @@ class OrdersTableSeeder extends Seeder
     public function run()
     {
         //
+        $user_ids = DB::table('users')->pluck('id');
+        $product_ids = DB::table('products')->pluck('id');
+        $number_of_products = rand(0, 5);
+        $user_index = rand(0, count($user_ids)-1);
+
+        $id = DB::table('orders')->insertGetId([
+            'user_id' => $user_ids[$user_index],
+            'total_price' => rand(0.1,100)
+        ]);
+
+        for ($i = 0; $i< $number_of_products ; $i++){
+            DB::table('order_products')->insert([
+                'order_id' => $id,
+                'product_id' => $product_ids[rand(0, count($product_ids)-1)],
+                'unit' => rand(1,10)
+            ]);
+        }
     }
 }
--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -12,5 +12,26 @@ class ProductsTableSeeder extends Seeder
     public function run()
     {
         //
+        $typeRand = rand(0, 1);
+        $type = "product";
+        if ($typeRand){
+            $type = "normal";
+        }else{
+            $type = "sale";
+        }
+        $id = DB::table('products')->insertGetId([
+            'name' => str_random(20),
+            'price' => rand(0, 1000),
+            'image_path' => str_random(20),
+            'type' => $type,
+        ]);
+        if($typeRand){
+            DB::table('normal_items')->insert(['id'=>$id,]);
+        }else{
+            DB::table('sale_items')->insert([
+                'id'=>$id,
+                'new_price' => rand(0, 1000),
+            ]);
+        }
     }
 }
